66862bb5941a7d653dc3d8702e56a4f63ed2c8d8
Diego Feijó Chagas <diego.feijo5@gmail.com>
2025-10-21 16:34:15 -0300

    backend home funcional

--- src/app/(panel)/Home/index.tsx
+++ src/app/(panel)/Home/index.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, Image, Pressable, FlatList, Alert } from 'react-native';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
@@ -11,20 +11,20 @@ import { styles } from '../../../styles/home/styles';
 import { FilterStatus } from '@/types/FilterStatus';
 import { supabase } from '@/lib/supabase';
 import { useAuth } from '@/contexts/AuthContexts';
+import { itemsStorage, ItemStorage } from '@/storage/itemsStorage';
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
-
-const ITEMS = [
-  {id: "1", status: FilterStatus.DONE, description: "1 pacote de café"},
-  {id: "2", status: FilterStatus.PENDING, description: "3 pacotes de macarrão"},
-  {id: "3", status: FilterStatus.DONE, description: "3 cebolas"}
-]
-
 
 
 
 export default function Home() {
   const { setAuth } = useAuth();
+  //Configurações do Filtro
+  const [filter, setFilter] = useState(FilterStatus.PENDING);
+  //Estado do input
+  const [description, setDescription] = useState("");
+
+  const [items, setItems] = useState<ItemStorage[]>([]);
   
   async function handleSignout() {
     const { error } = await supabase.auth.signOut();
@@ -36,12 +36,80 @@ export default function Home() {
     setAuth(null);
   }
 
-  //Configurações do Filtro
-  const [filter, setFilter] = useState(FilterStatus.PENDING)
-
   function update(value: FilterStatus){
     setFilter(value)
   }
+
+  async function handleAdd(){
+    if(!description.trim()){
+      return Alert.alert("Adicionar", "Informe a descrição para adicionar")
+    }
+    const newItem = {
+      id: Math.random().toString(36).substring(2),
+      description,
+      status: FilterStatus.PENDING,
+    }
+
+    await itemsStorage.add(newItem);
+    await itemsByStatus();  
+
+    Alert.alert("Adicionado", `Adicionado ${description}`);
+    setFilter(FilterStatus.PENDING);
+
+    setDescription("");
+    
+  }
+
+  async function itemsByStatus() {
+    try {
+      const response = await itemsStorage.getByStatus(filter)
+      setItems(response)
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Erro", "Não foi possível filtrar os itens.")
+    }
+  }
+
+  async function handelRemove(id:string) {
+    try {
+      await itemsStorage.remove(id);
+      await itemsByStatus();
+    } catch (error) {
+      Alert.alert("Remover", "Não foi possível remover")
+    }
+  }
+
+  function handleClear(){
+    Alert.alert("Limpar", "Deseja remover todos?", [
+      { text: "Não", style: "cancel" },
+      { text: "Sim", onPress: () => onClear() }
+    ])
+  }
+
+  async function onClear() {
+    try {
+      await itemsStorage.clear()
+      setItems([])
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Erro", 'Não foi possível remover todos os itens.')
+    }
+  }
+
+  async function handleToggleItemStatus(id:string){
+    try {
+      await itemsStorage.toggleStatus(id)
+      await itemsByStatus()
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Erro", "Não foi possível atualizar o status.")
+    }
+  }
+
+useEffect(() => {
+  itemsByStatus()
+}
+,[filter])  
 
 
   return (
@@ -51,8 +119,12 @@ export default function Home() {
               onPress={handleSignout}
       />
       <View style={styles.form}>
-        <Input placeholder='O que você precisa comprar?'/>
-        <Button title='Adicionar'/>
+        <Input 
+        placeholder='O que você precisa comprar?'
+        onChangeText={setDescription}
+        value={description}
+        />
+        <Button title='Adicionar' onPress={handleAdd}/>
       </View>
       
       <View style={styles.content}>
@@ -68,18 +140,18 @@ export default function Home() {
           ))
         }
 
-          <Pressable style={styles.clearButton}>
+          <Pressable style={styles.clearButton} onPress={handleClear}>
             <Text style={styles.clearText}>Limpar</Text>
           </Pressable>
         </View>
         <FlatList
-        data={ITEMS}
+        data={items}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <Item 
            data={ item }
-           onRemove={() => console.log("remover") }
-           onStatus={() => console.log("trocar status")}
+           onRemove={() => handelRemove(item.id)}
+           onStatus={() => handleToggleItemStatus(item.id)}
         />)}
         />
         
